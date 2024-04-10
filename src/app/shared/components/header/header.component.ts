c2b0cc6ac945735aad663c22584e34e4e59ddcd4
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-04-09 22:37:23 -0500

    Back button

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
-  @Input() title!:string;
+  @Input() title!: string;
+  @Input() backButton!: string;
+  @Input() isModal!: boolean;
+  @Input() showMenu!: boolean;
+
+  utilsSvc = inject(UtilsService);
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {} 
+  
+  dismissModal(){
+    //this.utilsSvc.dismisModal();
+  }
 
 }
