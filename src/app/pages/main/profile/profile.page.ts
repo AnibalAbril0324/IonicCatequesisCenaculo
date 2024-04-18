8f2ac4d5e9bf4deae670035e5887d8250e230f64
AnibalAbrilDJ <anibalabril0324@gmail.com>
2024-04-17 19:01:47 -0500

    Desarrollo del perfil

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  user(): User{
+    return this.utilsSvc.getFromLocalStorage('user');
+  }
+
+  //=============tomar/seleccionar una imagen del usuario========= 
+  async takeImage(){
+    let user=this.user();
+    let path= `users/${user.uid}`
+      
+    const dataUrl= (await this.utilsSvc.takePicture('Imagen del Perfil')).dataUrl;
+
+    const loading=await this.utilsSvc.loading();
+    await loading.present();
+
+    let imagePath= `${user.uid}/profile`;
+    user.image = await this.firebaseSvc.uploadImage(imagePath,dataUrl); 
+
+    this.firebaseSvc.updateDocument(path,{image: user.image}).then( async res => {
+        
+      this.utilsSvc.saveInLocalStorage('user',user);
+
+      this.utilsSvc.presentToast({
+        message: 'Imagen Actualizada Exitosamente',
+        duration: 1500,
+        color:'success',
+        position:'middle',
+        icon: 'checkmark-circle-outline'
+      });
+
+    }).catch(error => {
+      console.log(error);
+      this.utilsSvc.presentToast({
+        message: 'ERROR  EL CORREO O CONTRASEÑA SON INCORRECTOS',
+        duration: 2500,
+        color:'primary',
+        position:'middle',
+        icon: 'alert-circle'
+      });
+
+    }).finally( () => {
+      loading.dismiss();
+    })
+  }
 }
